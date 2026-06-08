5d4e3c032df2d5004dd55bdc74dc86c68fc633bc
Arbaj Shaikh <118815119+Arbajsshaikh@users.noreply.github.com>
2026-06-08 18:10:47 +0530

    Update LMSApp_Fixed.jsx

--- src/LMSApp_Fixed.jsx
+++ src/LMSApp_Fixed.jsx
@@ -4898,7 +4898,16 @@ function QuizTab({ day, dayData, busy, onGenQuiz, updateDay, notify, studentMode
           </div>
 
           {!submitted && (
-            <button className="lms-btn lms-btn-dark" style={{ marginTop:16, width:"100%", justifyContent:"center" }}
+            <button
+  className="lms-btn lms-btn-dark"
+  style={{
+    marginTop: 16,
+    width: "100%",
+    justifyContent: "center",
+    padding: "12px 0",
+    marginBottom: 90
+  }}
+>
               onClick={handleSubmit}>
               <Ic n="check" s={15}/>Submit Quiz
             </button>
